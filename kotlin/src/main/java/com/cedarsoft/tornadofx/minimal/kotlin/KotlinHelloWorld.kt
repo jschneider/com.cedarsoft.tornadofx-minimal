c16f533d9267c5703b257428cab2bc4bcf8d7736
Johannes Schneider <js@cedarsoft.com>
2017-11-23 09:32:18 +0100

    add mminimal kotlin hello world

---
+++ kotlin/src/main/java/com/cedarsoft/tornadofx/minimal/kotlin/KotlinHelloWorld.kt
@@ -0,0 +1,8 @@
+package com.cedarsoft.tornadofx.minimal.kotlin
+
+/**
+ * @author Johannes Schneider ([js@cedarsoft.com](mailto:js@cedarsoft.com))
+ */
+fun main(args: Array<String>) {
+  println("Hello World")
+}
